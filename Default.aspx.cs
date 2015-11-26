4900877c8939c24af92aa194c4c7de5b83477791
juliusbacosa <juliusbacosa@gmail.com>
2015-11-26 19:26:50 +0800

    - Lambda and Extension Methods in C#

---
+++ Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LanguageFeatures
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected string GetMessage()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product> {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports",
+                Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer",
+                Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer",
+                Price = 34.95M}
+                }
+            };
+            
+            decimal total = products.Filter(prod => prod.Category == "Soccer").TotalPrices();
+            return String.Format("Soccer Total: {0:c}", total);
+        }
+    }
+}
